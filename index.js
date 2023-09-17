8a269c4288edd2568c3ea220f299a1d770f97a6b
Ehigiepaul01 <paulehigie5@gmail.com>
2023-09-17 11:11:18 +0100

    linked the social icons to the social media accounts

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ hamburger.addEventListener("click", ()=> {
     navList.classList.toggle("show")
 })
 
+
+  // Find the button element by its ID
+  const downloadButton = document.getElementById('downloadButton');
+  
+  // Define the file URL
+  const fileURL = 'https://example.com/path-to-your-file.pdf';
+  
+  // Add a click event listener to the button
+  downloadButton.addEventListener('click', function() {
+    // Create an anchor element
+    const link = document.createElement('a');
+    link.href = fileURL;
+    link.target = '_blank'; // Open the link in a new tab (optional)
+    link.download = 'filename.pdf'; // Specify the file name to be downloaded
+    link.click(); // Simulate a click on the anchor element
+  });
+
+
   
